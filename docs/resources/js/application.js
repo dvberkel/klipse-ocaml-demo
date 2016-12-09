1272f319c8d215a0e4b42f8b6b747b28521cb599
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-12-09 21:23:09 +0100

    Evaluate generated code

--- docs/resources/js/application.js
+++ docs/resources/js/application.js
@@ -3,7 +3,9 @@
     var compiled_source = ocaml.compile(input.textContent);
     var compiled_object = JSON.parse(compiled_source);
 
-
     var output = document.getElementById('output');
     output.textContent = compiled_object['js_code'];
+
+    var code = "(function(exports){\n" + compiled_object['js_code'] + "\n})({});";
+    eval(code);
 })();
